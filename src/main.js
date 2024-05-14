ce2b1d50785d15000f795764198ccc2b86a657f4
Roman <morozov9786@gmail.com>
2024-05-14 20:55:12 +0300

    Создаст отдельный презентер для фильтров.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
+import FilterPresenter from './presenter/filter-presenter.js';
 import ListPresenter from './presenter/list-presenter.js';
 import PointsModel from './model/point-model.js';
 
+const filtersElement = document.querySelector('.trip-controls__filters');
 const eventTripElement = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
 
+const filterPresenter = new FilterPresenter({filterContainer: filtersElement});
 const listPresenter = new ListPresenter({listContainer: eventTripElement,
   pointsModel,
 });
 
+filterPresenter.init();
 listPresenter.init();
