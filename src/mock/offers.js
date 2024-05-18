e83203833065ee9a1a2ba82c3c3ec50d973c11f7
Roman <morozov9786@gmail.com>
2024-05-18 18:11:29 +0300

    Сделает новые временные данные.

--- src/mock/offers.js
+++ src/mock/offers.js
@@ -1,61 +1,163 @@
-import {TYPES_POINTS, OFFERS} from '../const.js';
-import {getRandomElement} from '../utils.js';
-
-export const mockOffers = [
+export const MOCK_OFFERS = [
   {
-    type: getRandomElement(TYPES_POINTS),
-    'offers': [{
-      id: '1',
-      title: getRandomElement(OFFERS),
-      price: Math.floor(Math.random() * 200)
-    }]
+    type: 'taxi',
+    'offers': [
+      {
+        id: '2569b95',
+        title: 'Switch to comfort',
+        price: 40
+      },
+      {
+        id: 'af5d844',
+        title: 'Seat for a pet',
+        price: 5
+      }
+    ]
   },
   {
-    type: getRandomElement(TYPES_POINTS),
-    'offers': [{
-      id: '2',
-      title: getRandomElement(OFFERS),
-      price: Math.floor(Math.random() * 200)
-    }]
+    type: 'bus',
+    'offers': [
+      {
+        id: 'f14f05d',
+        title: 'Switch to comfort',
+        price: 80
+      },
+      {
+        id: '293bc6b',
+        title: 'Add luggage',
+        price: 30
+      },
+      {
+        id: '792cd5f',
+        title: 'Window seat',
+        price: 45
+      },
+    ]
   },
   {
-    type: getRandomElement(TYPES_POINTS),
-    'offers': [{
-      id: '3',
-      title: getRandomElement(OFFERS),
-      price: Math.floor(Math.random() * 200),
-    }]
+    type: 'train',
+    'offers': [
+      {
+        id: '3322c52',
+        title: 'Switch to comfort',
+        price: 100
+      },
+      {
+        id: 'c551118',
+        title: 'Choose seats',
+        price: 5
+      },
+      {
+        id: '5163da3',
+        title: 'Add breakfast',
+        price: 15
+      },
+      {
+        id: 'd524497',
+        title: 'An air-conditioned carriage',
+        price: 70
+      },
+      {
+        id: '053bcd0',
+        title: 'Seat for a pet',
+        price: 22
+      },
+    ]
   },
   {
-    type: getRandomElement(TYPES_POINTS),
-    'offers': [{
-      id: '4',
-      title: getRandomElement(OFFERS),
-      price: Math.floor(Math.random() * 200),
-    }]
+    type: 'ship',
+    'offers': [
+      {
+        id: 'e244371',
+        title: 'Add a meal',
+        price: 40
+      },
+      {
+        id: '12cb95f',
+        title: 'Switch to comfort',
+        price: 100
+      },
+      {
+        id: '8d4ed52',
+        title: 'Seat for a pet',
+        price: 45
+      },
+      {
+        id: '1ffaccd',
+        title: 'Add cleaning',
+        price: 15
+      }
+    ]
   },
   {
-    type: getRandomElement(TYPES_POINTS),
-    'offers': [{
-      id: '5',
-      title: getRandomElement(OFFERS),
-      price: Math.floor(Math.random() * 200),
-    }]
+    type: 'drive',
+    'offers': [
+      {
+        id: '9f0e36a',
+        title: 'Toll roads',
+        price: 20
+      },
+      {
+        id: '40793ee',
+        title: 'Сhoose a car',
+        price: 50
+      }
+    ]
   },
   {
-    type: getRandomElement(TYPES_POINTS),
-    'offers': [{
-      id: '6',
-      title: getRandomElement(OFFERS),
-      price: Math.floor(Math.random() * 200),
-    }]
+    type: 'flight',
+    'offers': [
+      {
+        id: 'ac5d23b',
+        title: 'Switch to business',
+        price: 99
+      },
+      {
+        id: '546f8ad',
+        title: 'Choose seats',
+        price: 25
+      },
+      {
+        id: 'b0b3aeb',
+        title: 'Add luggage',
+        price: 12
+      }
+    ]
   },
   {
-    type: getRandomElement(TYPES_POINTS),
-    'offers': [{
-      id: '7',
-      title: getRandomElement(OFFERS),
-      price: Math.floor(Math.random() * 200),
-    }]
+    type: 'check-in',
+    'offers': []
+  },
+  {
+    type: 'sightseeing',
+    'offers': [
+      {
+        id: '7858dc0',
+        title: 'Order Uber',
+        price: 55
+      },
+      {
+        id: '7db9aec',
+        title: 'Add excursion',
+        price: 15
+      },
+      {
+        id: '73d1c07',
+        title: 'Scooter rental',
+        price: 7
+      }
+    ]
+  },
+  {
+    type: 'restaurant',
+    'offers': [
+      {
+        id: 'd1a1d57',
+        title: 'Paid parking',
+        price: 10
+      }
+    ]
   }
 ];
+
+
