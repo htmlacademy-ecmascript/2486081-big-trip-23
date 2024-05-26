ff6cd68f506bec03aa8d541c612021850ee21c4a
Roman <morozov9786@gmail.com>
2024-05-26 14:59:42 +0300

    6.8. Обновление века (часть 1)

--- src/view/edit-point-view/index.js
+++ src/view/edit-point-view/index.js
@@ -1,8 +1,7 @@
-import AbstractView from '../../framework/view/abstract-view';
+import AbstractStatefulView from '../../framework/view/abstract-stateful-view';
 import {createEditPointTemplate} from './template';
 
-export default class EditPointView extends AbstractView {
-  #points = null;
+export default class EditPointView extends AbstractStatefulView {
   #offers = null;
   #destinations = null;
 
@@ -10,21 +9,48 @@ export default class EditPointView extends AbstractView {
 
   constructor({points, offers, destinations, onFormSubmit}) {
     super();
-    this.#points = points;
+    this._setState(EditPointView.parsePointToState(points));
     this.#offers = offers;
     this.#destinations = destinations;
 
     this.#onFormSubmit = onFormSubmit;
-    this.element.querySelector('form').addEventListener('submit', this.#formSubmitHandler);
 
+    this._restoreHandlers();
   }
 
   get template() {
-    return createEditPointTemplate(this.#points, this.#offers, this.#destinations);
+    return createEditPointTemplate(this._state, this.#offers, this.#destinations);
+  }
+
+  _restoreHandlers() {
+    this.element.querySelector('form').addEventListener('submit', this.#formSubmitHandler);
+    this.element.querySelector('.event__type-group').addEventListener('change', this.#typeChangeHandler);
+    this.element.querySelector('.event__input--destination').addEventListener('change', this.#destinationChangeHandler);
   }
 
   #formSubmitHandler = (evt) => {
     evt.preventDefault();
-    this.#onFormSubmit(this.#points);
+    this.#onFormSubmit(EditPointView.parseStateToPoint(this._state));
+  };
+
+  static parsePointToState(points) {
+    return {...points};
+  }
+
+  static parseStateToPoint(state) {
+    const points = {...state};
+    return points;
+  }
+
+  #typeChangeHandler = (evt) => {
+    this.updateElement({
+      type: evt.target.value
+    });
+  };
+
+  #destinationChangeHandler = (evt) => {
+    this.updateElement({
+      destination: this.#destinations.find(({name}) => name === evt.target.value).id
+    });
   };
 }
