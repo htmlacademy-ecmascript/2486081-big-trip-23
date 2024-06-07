c19fadc7e05ed3c6e39744d92bb8b8aa33a45de3
Roman <morozov9786@gmail.com>
2024-06-07 23:58:08 +0300

    Сделает сообщение когда нет поинтов.

---
+++ src/view/no-point-view/index.js
@@ -0,0 +1,15 @@
+import AbstractView from '../../framework/view/abstract-view';
+import { createNoPointTemplate } from './template';
+export default class NoPointView extends AbstractView {
+  #filterType = null;
+
+  constructor({filterType}) {
+    super();
+    this.#filterType = filterType;
+  }
+
+  get template() {
+    return createNoPointTemplate(this.#filterType);
+  }
+}
+
