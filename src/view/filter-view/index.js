ac82f472bc04f312b687d5038fdb45e46d4c7997
Roman <morozov9786@gmail.com>
2024-06-07 23:55:13 +0300

    Сделает фильтры.

--- src/view/filter-view/index.js
+++ src/view/filter-view/index.js
@@ -2,7 +2,25 @@ import AbstractView from '../../framework/view/abstract-view';
 import {createFiltersTemplate} from './template';
 
 export default class FiltersView extends AbstractView {
-  get template() {
-    return createFiltersTemplate();
+  #filters = null;
+  #currentFilter = null;
+  #onFilterChange = null;
+
+  constructor({filters, currentFilter, onFilterChange}) {
+    super();
+    this.#filters = filters;
+    this.#currentFilter = currentFilter;
+    this.#onFilterChange = onFilterChange;
+
+    this.element.addEventListener('change', this.#filtersChangeHandler);
   }
+
+  get template() {
+    return createFiltersTemplate(this.#filters, this.#currentFilter);
+  }
+
+  #filtersChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.#onFilterChange(evt.target.value.toUpperCase());
+  };
 }
--- src/view/filter-view/template.js
+++ src/view/filter-view/template.js
@@ -1,27 +1,11 @@
-export function createFiltersTemplate() {
+export function createFiltersTemplate(filter, currentFilter) {
   return (
     `<form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-        <label class="trip-filters__filter-label" for="filter-present">Present</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" checked>
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
-
-      <button class="visually-hidden" type="submit">Accept filter</button>
+    ${filter.map((type) =>
+      `<div class="trip-filters__filter">
+        <input id="filter-${type.type.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type.type.toLowerCase()}" ${type.type === currentFilter ? 'checked' : ''}>
+        <label class="trip-filters__filter-label" for="filter-${type.type.toLowerCase()}">${type.type}</label>
+      </div>`).join('')}
     </form>`
   );
 }
