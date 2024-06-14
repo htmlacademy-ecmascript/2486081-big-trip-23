58e7185d05b02dad5827c7e1bd23dfd41003b0e7
Roman <morozov9786@gmail.com>
2024-06-14 21:41:43 +0300

    Исправит разницу между датами

--- src/view/route-point-view/utils.js
+++ src/view/route-point-view/utils.js
@@ -1,17 +1,21 @@
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration);
 
 function differenceTime(end, start) {
-  const diff = dayjs(end).diff(dayjs(start), 'days', true);
-  const day = Math.floor(diff);
-  const hours = Math.floor((diff - day) * 24);
-  const min = Math.round((((diff - day) * 24) - hours) * 60);
-  if (hours === 0 && day === 0) {
-    return `${min}M`;
-  }
+  const diffDay = dayjs(end).diff(dayjs(start), 'days');
+  const day = dayjs.duration({day: diffDay}).format('DD[d]');
+  const hours = dayjs.duration(dayjs(end).diff(dayjs(start))).format('HH[h]');
+  const minute = dayjs.duration(dayjs(end).diff(dayjs(start))).format('mm[m]');
+
   if(day === 0) {
-    return `${hours}H ${min}M`;
+    return `${hours} ${minute}`;
   }
-  return `${day}D ${hours}H ${min}M`;
+  if (hours === 0 && day === 0) {
+    return `${minute}`;
+  }
+
+  return `${day} ${hours} ${minute}`;
 }
 
 export {differenceTime};
